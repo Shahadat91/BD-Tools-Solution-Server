5945b1f34046b583e0f48a17927b81cd3edbb9ec
Shahadat91 <shahadat20000@gmail.com>
2022-05-29 22:39:03 +0600

    delete api update

--- index.js
+++ index.js
@@ -100,13 +100,13 @@ app.post('/login', async(req, res)=>{
         });
 
         //  //order delete api
-        //  app.delete('/order/:id', async(req, res) => {
-        //     const id = req.params.id;
-        //     const query = {_id: ObjectId(id)};
-        //     const result = await orderCollection.deleteOne(query);
-        //     res.send(result);
+         app.delete('/order/:id', async(req, res) => {
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const result = await orderCollection.deleteOne(query);
+            res.send(result);
 
-        // });
+        });
 
        
 
